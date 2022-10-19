1d4064e79578dfd9ae0e3bbab99c476eedc13eaa
sumanth <sumanthsummi876@gmail.com>
2022-10-19 15:05:27 +0530

    a score

---
+++ index.js
@@ -0,0 +1,38 @@
+let home = 0,
+    guest = 0;
+
+function one() {
+    home = home + 1;
+    document.getElementById("home").innerHTML = home;
+
+}
+
+function two() {
+    home = home + 2;
+    document.getElementById("home").innerHTML = home;
+
+}
+
+function three() {
+    home = home + 3;
+    document.getElementById("home").innerHTML = home;
+
+}
+
+function oneg() {
+    guest = guest + 1;
+    document.getElementById("guest").innerHTML = guest;
+
+}
+
+function twog() {
+    guest += 2;
+    document.getElementById("guest").innerHTML = guest;
+
+}
+
+function threeg() {
+    guest += 3;
+    document.getElementById("guest").innerHTML = guest;
+
+}
